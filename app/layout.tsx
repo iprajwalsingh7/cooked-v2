85e46093ecf849843a8a2d68a67f8946a9ff4c29
iprajwalsingh7 <prajwalsingh0605@gmail.com>
2025-12-01 14:23:57 +0530

    Update license to proprietary and cleanup project files

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
             </head>
             <body>
                 {children}
+
                 <Analytics />
                 <SpeedInsights />
             </body>
